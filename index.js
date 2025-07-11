ed66cca1b8ff47eec985d213eae7d82e9b15eeea
siddharthaasal <siddharthaasal@gmail.com>
2025-07-12 03:05:14 +0530

    core: rename files in sub-dirs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ try {
 
 // create subdir if it does not exist, and move files to it
 
-// if the dump folder has sub-dir, ignore them, jsut read the root level files
+// if the dump folder has sub-dir, ignore them, just read the root level files
 
 fs.readdirSync(dumpFolderPath)
     .map(fileName => {
@@ -40,14 +40,14 @@ fs.readdirSync(dumpFolderPath)
 
             // create subdir if it does not exist
             let subDir = fileType;
-            const targetDir = `${dumpFolderPath}/${subDir}`;
+            const targetDir = `${dumpFolderPath}/_${subDir}_`;
             if (!fs.existsSync(targetDir)) {
                 fs.mkdirSync(targetDir);
             }
 
             // move the file to the subdir
             fs.renameSync(`${dumpFolderPath}/${fileName}`, `${targetDir}/${fileName}`);
-            console.log(`Moved ${fileName} to ${subDir}`);
+            // console.log(`Moved ${fileName} to ${subDir}`);
 
         } else {
             console.log(`Skipping directory: ${fileName}`);
@@ -56,6 +56,39 @@ fs.readdirSync(dumpFolderPath)
 
 // rename files in subdir
 
+// get all subdirectories in the dump folder
+const possibleSubDirs = ['_images_', '_videos_', '_audio_', '_documents_', '_others_'];
+const subDirs = fs.readdirSync(dumpFolderPath).
+    filter(fileName => !isFile(`${dumpFolderPath}/${fileName}`))
+    .filter(fileName => possibleSubDirs.includes(fileName));
 
+console.log("Subdirectories found:", subDirs);
+
+// rename files in each subdir
+
+subDirs.map(dir => {
+    const dirPath = `${dumpFolderPath}/${dir}`;
+    console.log(`Processing directory: ${dirPath}`);
+    let fileCount = 1;
+    let fileType = dir.replace(/_/g, '').toLowerCase(); // remove underscores and convert to lowercase
+    fs.readdirSync(dirPath).forEach(fileName => {
+        const filePath = `${dirPath}/${fileName}`;
+        if (isFile(filePath)) {
+            // remove special characters from the file name
+            const newFileName = fileName.replace(/[^a-zA-Z0-9._-]/g, `${fileCount++}_${fileType}`);
+            const newFilePath = `${dirPath}/${newFileName}`;
+
+            // rename the file if the name has changed
+            if (newFileName !== fileName) {
+                fs.renameSync(filePath, newFilePath);
+                // console.log(`Renamed ${fileName} to ${newFileName}`);
+            } else {
+                // console.log(`No renaming needed for ${fileName}`);
+            }
+        } else {
+            console.log(`Skipping non-file item: ${fileName}`);
+        }
+    });
+})
 
 console.log("eop");
